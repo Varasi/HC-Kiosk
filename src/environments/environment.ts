4ae6b54dcc6c61faa16f0a91562c3cd50b1abea2
Shubham Mangrule <shubham.mangrule@varasi.com>
2024-12-03 05:36:52 +0530

    Made chnages in index.html to accept google api key at build time

---
+++ src/environments/environment.ts
@@ -0,0 +1,30 @@
+export const environment = {
+    production: true,
+    
+    version: '',
+    release_date: '',
+
+    deployment: 'HIRTA',
+    appName: 'KIOSK',
+
+    trip_origin: '',
+
+    kiosk_lat: '',
+    kiosk_lng: '',
+    kiosk_address: '',
+
+    hirta_via_account_name: 'Health_Connector',
+
+    southwestLat: 40.62844733741235, 
+    southwestLng: -95.51470502550431,
+    northeastLat: 42.94950757503583, 
+    northeastLng: -91.7188823451054,
+    REGION : '',
+    POOL_ID:'',
+    CLIENT_ID: '',
+    IDENTITY_POOL_ID : '',
+    PASSWORD_REGEX : /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    GOOGLE_MAPS_KEY:'',
+
+        api_url: ''
+};
